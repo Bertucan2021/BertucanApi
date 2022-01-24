55fb96572e31d2e1ce2be699ca87215b4908199a
edenAbdisa <eden.abdisa@yahoo.com>
2022-01-24 17:09:09 +0300

    Model

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Membership extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'type',
+        'show_ad',
+        'add_per_minute',
+        'allowed_add_per_month'
+    ];
 }
