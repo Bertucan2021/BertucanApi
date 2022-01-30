17bbce23e3e595ca1300da74dfc0ebff26d3bc81
edenAbdisa <eden.abdisa@yahoo.com>
2022-01-30 11:00:42 +0300

    rename classes

--- database/migrations/2023_01_23_053218_create_log_infos_table.php
+++ database/migrations/2023_01_23_053218_create_log_infos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLogInfosTable extends Migration
+class CreateLogInfosTables extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2023_01_23_053624_create_media_table.php
+++ database/migrations/2023_01_23_053624_create_media_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMediaTable extends Migration
+class CreateMediaTables extends Migration
 {
     /**
      * Run the migrations.
