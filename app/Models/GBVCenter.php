0bc5957d4d2d804a91469f502c1008fe9268b165
edenAbdisa <eden.abdisa@yahoo.com>
2022-03-09 20:46:06 +0300

    gbv

--- app/Models/GBVCenter.php
+++ app/Models/GBVCenter.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GBVCenter extends Model
+class GbvCenter extends Model
 {
     use HasFactory;
     protected $fillable = [
