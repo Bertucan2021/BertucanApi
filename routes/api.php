8fdc0bf736b1a8603bd81342bd5d84ec593b6b1d
edenAbdisa <edenabdisa90@gmail.com>
2022-07-09 21:55:31 +0300

    Fix in fetch all

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('/users', 'UserController@store');
-Route::middleware(['auth:api', 'scope:user'])->group(function () {
+Route::middleware(['auth:api', 'scope:user'])->middleware('bindings')->group(function () {
     Route::get('/users', 'UserController@index');
     Route::get('/articles', 'ArticleController@index');
 
@@ -28,7 +28,7 @@ Route::middleware(['auth:api', 'scope:user'])->group(function () {
     Route::get('/cycleHistories', 'CycleHistoryController@index');
     Route::get('/cycleHistories/{id}', 'CycleHistoryController@show');
     Route::put('/cycleHistories', 'CycleHistoryController@update');
-    Route::delete('/cycleHistories', 'CycleHistoryController@destroy')->middleware('bindings');
+    Route::delete('/cycleHistories', 'CycleHistoryController@destroy');
 
     Route::post('/logInfos', 'LogInfoController@store');
     Route::get('/logInfos', 'LogInfoController@index');
